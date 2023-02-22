c5c76bea5a4db3cbebb1af33fe24f451452ca682
Natalia Loskutova <loskutovanl@gmail.com>
2023-02-22 07:52:01 +0300

    initial work on methods advancedMoveValidation, Advanced and getNewBoard; corrections to VALIDATION.md regarding statusBadRequest 400 in case of invalid input

--- validation/controllers.go
+++ validation/controllers.go
@@ -4,6 +4,7 @@ package validation
 
 import (
 	"fmt"
+	"github.com/sadmadrus/chessBox/internal/usfen"
 	"net/http"
 	"strconv"
 
@@ -27,6 +28,7 @@ var (
 	errClashWithPieceOfSameColor = fmt.Errorf("clash with piece of the same color")
 	errClashWithKing             = fmt.Errorf("clash with king")
 	errClashWithPawn             = fmt.Errorf("pawn can not clash with another piece when moving vertically")
+	errPawnPromotionNotValid     = fmt.Errorf("pawn promotion to pawn or king is not valid")
 )
 
 // http хендлеры
@@ -110,16 +112,110 @@ func Simple(w http.ResponseWriter, r *http.Request) {
 
 // Advanced сервис отвечает за сложную валидацию хода по начальной и конечной клетке, а также по текущему состоянию
 // доски в нотации FEN. Также принимает на вход URL-параметр newpiece (это новая фигура, в которую нужно превратить
-// пешку при достижении последнего ряда, в формате pieceВозвращает заголовок HttpResponse 200 (ход валиден) или
-// HttpsResponse 403 (ход невалиден). Возвращает в теле JSON с конечной доской board в форате FEN.
+// пешку при достижении последнего ряда), в формате pieceВозвращает заголовок HttpResponse 200 (ход валиден) или
+// HttpsResponse 403 (ход невалиден). Возвращает HttpResponse 400 при некорректном методе запроса и некорректных
+// входных данных. Возвращает в теле JSON с конечной доской board в форате FEN.
+// Входящие URL параметры:
+// * доска board в формате UsFen (например, "rnbqkbnr~pppppppp~8~8~8~8~PPPPPPPP~RNBQKBNR+w+KQkq+-+0+1")
+// * начальная клетка предполагаемого хода from (число от 0 до 63, либо строка вида a1, c7 и т.п)
+// * конечная клетка предполагаемого хода to (число от 0 до 63, либо строка вида a1, c7 и т.п).
+// * фигура newpiece (q/r/b/n/Q/R/B/N или пустое значение)
 func Advanced(w http.ResponseWriter, r *http.Request) {
 	// TODO написать логику
+	if r.Method == "GET" || r.Method == "HEAD" {
+		// валидация входных данных: доска board существует
+		// TODO board нужно ли проверять валидность позиции?
+		boardParsed := r.URL.Query().Get("board")
+		boardFen := usfen.ToFen(boardParsed)
+		b, err := board.FromFEN(boardFen)
+		if err != nil {
+			log.Errorf("%v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// валидация входных данных: клетка from существуют
+		fromParsed := r.URL.Query().Get("from")
+		// перевод в тип board.square для цифро-буквенного обозначения клетки (напр., "а1")
+		from := board.Sq(fromParsed)
+		if from == -1 {
+			// перевод в тип board.square для числового значения клетки от 0 до 63
+			var fromParsedNum int
+			fromParsedNum, err = strconv.Atoi(fromParsed)
+			from = board.Sq(fromParsedNum)
+			if from == -1 || err != nil {
+				log.Errorf("%v: %v", errPieceNotExist, fromParsed)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		// валидация входных данных: клетка to существуют
+		toParsed := r.URL.Query().Get("to")
+		// перевод в тип board.square для цифро-буквенного обозначения клетки (напр., "а1")
+		to := board.Sq(toParsed)
+		if to == -1 {
+			// перевод в тип board.square для числового значения клетки от 0 до 63
+			var toParsedNum int
+			toParsedNum, err = strconv.Atoi(toParsed)
+			to = board.Sq(toParsedNum)
+			if to == -1 || err != nil {
+				log.Errorf("%v: %s", errPieceNotExist, toParsed)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		// валидация входных данных: клетки from и to различны
+		if from == to {
+			log.Errorf("%v: %v (from), %v (to)", errFromToSquaresNotDiffer, from, to)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// валидация входных данных: фигура newpiece принимает q/r/b/n/Q/R/B/N или пустое значение
+		newpieceParsed := r.URL.Query().Get("newpiece")
+		var newpiece board.Piece
+		if newpieceParsed != "" {
+			newpiece, err = parsePieceFromLetter(newpieceParsed)
+			if err != nil {
+				log.Errorf("%v: %v", errPieceNotExist, newpieceParsed)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			switch newpiece {
+			case board.WhitePawn, board.BlackPawn, board.WhiteKing, board.BlackKing:
+				log.Errorf("%v: %v", errPawnPromotionNotValid, newpieceParsed)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		// полная валидация хода с учетом положения на доске, а также возможных рокировок, взятия на проходе и
+		// проведения пешки
+		fromSquare := newSquare(int8(from))
+		toSquare := newSquare(int8(to))
+		newBoard, err := advancedMoveValidation(*b, fromSquare, toSquare, newpiece)
+		if err != nil {
+			log.Errorf("move invalid: %v", err)
+			w.WriteHeader(http.StatusForbidden)
+			return
+		} else {
+			// TODO возвращать в теле новую доску
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+
+	log.Errorf("inside Advanced %v: %v", errInvalidHttpMethod, r.Method)
+	w.WriteHeader(http.StatusBadRequest)
 }
 
 // AvailableMoves сервис отвечает за оплучение всех возможных ходов для данной позиции доски в нотации FEN и начальной клетке.
 // Возвращает заголовок HttpResponse 200 (в случае непустого массива клеток) или HttpsResponse 403 (клетка пустая или
-// с фигурой, которой не принадлежит ход или массив клеток пуст). Возвращает в теле
-// JSON массив всех клеток, движение на которые валидно для данной фигуры.
+// с фигурой, которой не принадлежит ход или массив клеток пуст). Возвращает HttpResponse 400 при некорректном методе
+// запроса и некорректных входных данных. Возвращает в теле JSON массив всех клеток, движение на которые валидно
+// для данной фигуры.
 func AvailableMoves(w http.ResponseWriter, r *http.Request) {
 	// TODO написать логику
 }
@@ -162,59 +258,60 @@ func parsePieceFromLetter(piece string) (board.Piece, error) {
 }
 
 // advancedMoveValidation обрабывает общую логику валидации хода.
-func advancedMoveValidation(b board.Board, from, to int8) error {
+func advancedMoveValidation(b board.Board, from, to square, newpiece board.Piece) (newBoard board.Board, err error) {
 	// TODO написать логику
 	// Логика валидации хода пошагово.
 	// 1. получаем фигуру, находящуюся в клетке from. Если в этой клетке фигуры нет, возвращаем ошибку
-	piece, err := b.Get(board.Sq(int(from)))
+	var piece board.Piece
+	piece, err = b.Get(board.Sq(from.toInt()))
 	if err != nil {
-		return err
+		return newBoard, err
 	}
 	if piece == 0 {
-		return fmt.Errorf("%v: %v", errNoPieceOnFromSquare, from)
+		return newBoard, fmt.Errorf("%v: %v", errNoPieceOnFromSquare, from)
 	}
 
 	// 2. проверяем, что фигура принадлежит той стороне, чья очередь хода
 	isFigureRightColor := checkFigureColor(b, piece)
 	if !isFigureRightColor {
-		return fmt.Errorf("%v: %v", errPieceWrongColor, from)
+		return newBoard, fmt.Errorf("%v: %v", errPieceWrongColor, from)
 	}
 
 	// 3. Проверяем, что фигура в принципе может двигаться в этом направлении (f.e. диагонально для слона, и т.д.)
-	err = move(piece, newSquare(from), newSquare(to))
+	err = move(piece, from, to)
 	if err != nil {
-		return err
+		return newBoard, err
 	}
 
 	// 4. Проверяем, что по пути фигуры с клетки from до to (не включительно) нет других фигур
 	// (f.e. слон а1-h8, но на b2 стоит конь - так запрещено).
-	squaresToBePassed := getSquaresToBePassed(piece, newSquare(from), newSquare(to))
+	squaresToBePassed := getSquaresToBePassed(piece, from, to)
 	if len(squaresToBePassed) > 0 {
 		err = checkSquaresToBePassed(b, squaresToBePassed)
 		if err != nil {
-			return err
+			return newBoard, err
 		}
 	}
 
 	// 5. Проверяем наличие и цвет фигур в клетке to.
-	err = checkToSquare(&b, piece, newSquare(from), newSquare(to))
+	err = checkToSquare(&b, piece, from, to)
 	if err != nil {
-		return err
+		return newBoard, err
 	}
 
 	// TODO: остановилась здесь
 	// TODO: проверяем, что пользователь не захотел выставить нового ферзя в неуместном для этого случае.
 
 	// 6. На текущем этапе ход возможен. Генерируем новое положение доски newBoard.
-	err = b.Move(board.Sq(int(from)), board.Sq(int(to))) // TODO учесть промоушен пешки
+	newBoard, err = getNewBoard(b, piece, from, to, newpiece)
 	if err != nil {
-		return fmt.Errorf("move invalid: %w", err)
+		return newBoard, fmt.Errorf("move invalid: %w", err)
 	}
 
 	// 7. Проверяем, что при новой позиции на доске не появился шах для собственного короля оппонента o.
 	err = checkSelfCheck(b)
 	if err != nil {
-		return fmt.Errorf("move invalid: %w", err)
+		return newBoard, fmt.Errorf("move invalid: %w", err)
 	}
 
 	// 8. В случае если ход делается королем, проверяем, что он не подступил вплотную к чужому королю - такой ход
@@ -223,7 +320,7 @@ func advancedMoveValidation(b board.Board, from, to int8) error {
 	// 9. В случае если ход делается пешкой, проверяем, попала ли пешка на последнюю линию и потребуется ли
 	// трансформация в другую фигуру.
 
-	return nil
+	return newBoard, nil
 }
 
 // checkFigureColor проверяет, что очередь хода и цвет фигуры p, которую хотят передвинуть, совпадают.
@@ -327,6 +424,33 @@ func checkToSquare(b *board.Board, pieceFrom board.Piece, from, to square) error
 	}
 
 	return nil
+}
+
+// getNewBoard генерирует новое положение доски с учетом рокировок, взятия на прохоже и проведения пешки. Возвращает
+// ошибку при некорректных входных данных.
+func getNewBoard(b board.Board, piece board.Piece, from, to square, newpiece board.Piece) (board.Board, error) {
+	switch piece {
+	case board.WhitePawn:
+		if to.row == 7 {
+			err := b.Promote(board.Sq(from.toInt()), board.Sq(to.toInt()), newpiece)
+			if err != nil {
+				return b, err
+			}
+		}
+
+	case board.BlackPawn:
+		if to.row == 0 {
+			err := b.Promote(board.Sq(from.toInt()), board.Sq(to.toInt()), newpiece)
+			if err != nil {
+				return b, err
+			}
+		}
+
+		// TODO рокировка
+	case board.WhiteKing:
+
+	}
+
 }
 
 // checkSelfCheck проверяет, что при новой позиции на доске нет шаха собственному королю.
