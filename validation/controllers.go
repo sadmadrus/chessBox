0c76f35a69e57145f49c1841ee58e8602b0695cf
Natalia Loskutova <loskutovanl@gmail.com>
2023-02-25 10:53:33 +0300

    deleted everything for advanced validation

--- validation/controllers.go
+++ validation/controllers.go
@@ -3,7 +3,6 @@
 package validation
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/sadmadrus/chessBox/internal/board"
 	"log"
@@ -12,29 +11,15 @@ import (
 )
 
 var (
-	errPieceNotExist                = fmt.Errorf("piece does not exist")
-	errInvalidHttpMethod            = fmt.Errorf("method is not supported")
-	errFromToSquaresNotDiffer       = fmt.Errorf("from and to squares are not different")
-	errPawnMoveNotValid             = fmt.Errorf("pawn move is not valid")
-	errKnightMoveNotValid           = fmt.Errorf("knight move is not valid")
-	errBishopMoveNotValid           = fmt.Errorf("bishop move is not valid")
-	errRookMoveNotValid             = fmt.Errorf("rook move is not valid")
-	errQueenMoveNotValid            = fmt.Errorf("queen move is not valid")
-	errKingMoveNotValid             = fmt.Errorf("king move is not valid")
-	errNoPieceOnFromSquare          = fmt.Errorf("no piece on from square")
-	errPieceWrongColor              = fmt.Errorf("piece has wrong color")
-	errPieceFound                   = fmt.Errorf("piece found on the square")
-	errClashWithPieceOfSameColor    = fmt.Errorf("clash with piece of the same color")
-	errClashWithKing                = fmt.Errorf("clash with king")
-	errClashWithPawn                = fmt.Errorf("pawn can not clash with another piece when moving vertically")
-	errPawnPromotionNotValid        = fmt.Errorf("pawn promotion to pawn or king is not valid")
-	errNewpieceExist                = fmt.Errorf("newpiece exists with no pawn promotion")
-	errNewpieceNotExist             = fmt.Errorf("newpiece does not exist but pawn promotion required")
-	errPieceNotExistOnBoard         = fmt.Errorf("piece does not exist on board")
-	errKingChecked                  = fmt.Errorf("king checked after move")
-	errKingsAdjacent                = fmt.Errorf("kings are adjacent")
-	errCastlingThroughCheckedSquare = fmt.Errorf("castling is not valid through square under check")
-	errPiecesStayInTheWay           = fmt.Errorf("piece or pieces stay in the way of figure move")
+	errPieceNotExist          = fmt.Errorf("piece does not exist")
+	errInvalidHttpMethod      = fmt.Errorf("method is not supported")
+	errFromToSquaresNotDiffer = fmt.Errorf("from and to squares are not different")
+	errPawnMoveNotValid       = fmt.Errorf("pawn move is not valid")
+	errKnightMoveNotValid     = fmt.Errorf("knight move is not valid")
+	errBishopMoveNotValid     = fmt.Errorf("bishop move is not valid")
+	errRookMoveNotValid       = fmt.Errorf("rook move is not valid")
+	errQueenMoveNotValid      = fmt.Errorf("queen move is not valid")
+	errKingMoveNotValid       = fmt.Errorf("king move is not valid")
 )
 
 // http хендлеры
@@ -131,103 +116,7 @@ type advancedResponse struct {
 // * конечная клетка предполагаемого хода to (число от 0 до 63, либо строка вида a1, c7 и т.п).
 // * фигура newpiece (q/r/b/n/Q/R/B/N или пустое значение)
 func Advanced(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" || r.Method == "HEAD" {
-		// валидация входных данных: доска board существует
-		// TODO board нужно ли проверять валидность доски где-то вообще?
-		boardParsed := r.URL.Query().Get("board")
-		b, err := board.FromUsFEN(boardParsed)
-		if err != nil {
-			log.Printf("%v", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// валидация входных данных: клетка from существуют
-		fromParsed := r.URL.Query().Get("from")
-		// перевод в тип board.square для цифро-буквенного обозначения клетки (напр., "а1")
-		from := board.Sq(fromParsed)
-		if from == -1 {
-			// перевод в тип board.square для числового значения клетки от 0 до 63
-			var fromParsedNum int
-			fromParsedNum, err = strconv.Atoi(fromParsed)
-			from = board.Sq(fromParsedNum)
-			if from == -1 || err != nil {
-				log.Printf("%v: %v", errPieceNotExist, fromParsed)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		}
-
-		// валидация входных данных: клетка to существуют
-		toParsed := r.URL.Query().Get("to")
-		// перевод в тип board.square для цифро-буквенного обозначения клетки (напр., "а1")
-		to := board.Sq(toParsed)
-		if to == -1 {
-			// перевод в тип board.square для числового значения клетки от 0 до 63
-			var toParsedNum int
-			toParsedNum, err = strconv.Atoi(toParsed)
-			to = board.Sq(toParsedNum)
-			if to == -1 || err != nil {
-				log.Printf("%v: %s", errPieceNotExist, toParsed)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		}
-
-		// валидация входных данных: клетки from и to различны
-		if from == to {
-			log.Printf("%v: %v (from), %v (to)", errFromToSquaresNotDiffer, from, to)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// валидация входных данных: фигура newpiece принимает q/r/b/n/Q/R/B/N или пустое значение
-		newpieceParsed := r.URL.Query().Get("newpiece")
-		var newpiece board.Piece
-		if newpieceParsed != "" {
-			newpiece, err = parsePieceFromLetter(newpieceParsed)
-			if err != nil {
-				log.Printf("%v: %v", errPieceNotExist, newpieceParsed)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			switch newpiece {
-			case board.WhitePawn, board.BlackPawn, board.WhiteKing, board.BlackKing:
-				log.Printf("%v: %v", errPawnPromotionNotValid, newpieceParsed)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		}
-
-		// полная валидация хода с учетом положения на доске, а также возможных рокировок, взятия на проходе и
-		// проведения пешки
-		fromSquare := newSquare(int8(from))
-		toSquare := newSquare(int8(to))
-		newBoard, isValid, err := advancedLogic(*b, fromSquare, toSquare, newpiece)
-		if err != nil {
-			log.Printf("error occured when trying to validate move: %v", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if !isValid {
-			log.Printf("move invalid: %v", err)
-			w.WriteHeader(http.StatusForbidden)
-		} else {
-			boardUsFEN := newBoard.UsFEN()
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			data := advancedResponse{boardUsFEN}
-			err = json.NewEncoder(w).Encode(data)
-			if err != nil {
-				log.Printf("error while encoding json: %v", err)
-			}
-			return
-		}
-	}
-
-	log.Printf("inside Advanced %v: %v", errInvalidHttpMethod, r.Method)
-	w.WriteHeader(http.StatusBadRequest)
+	// TODO
 }
 
 // AvailableMoves сервис отвечает за оплучение всех возможных ходов для данной позиции доски в нотации FEN и начальной клетке.
